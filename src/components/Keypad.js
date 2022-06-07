bfda37195864b6bcc2d8b832fd7bc09670ad3ef6
Brendan Ward <kingbeebop@gmail.com>
2022-06-07 09:58:55 -0400

    commit tree abb17d7e530bafc53962e5c1a5351aac70eea441

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+
+function Keypad(){
+
+    function enterPassword(e){
+        console.log("Entering password...")
+    }
+
+    return(
+        <input type="password" onChange={enterPassword}/>
+    )
+}
+
+
+
+export default Keypad
